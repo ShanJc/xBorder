8dd7c06f513903e5492cd8ddd062baef66f7b970
Bigfa <bigfa@pku.edu.cn>
2013-02-04 01:21:44 +0800

    setting fixed

--- functions/themeset.php
+++ functions/themeset.php
@@ -219,8 +219,7 @@ $(".toggle").click(function(){$(this).next().slideToggle('slow')});
 						<div class="none">
 							<strong>网站描述</strong> （用简洁干练的话对你的网站进行描述）<br/><label><textarea name="description_content"  rows="4"  id="description_content" style="width:540px;"  ><?php echo($options['description_content']); ?></textarea></label>
 							<br/><br/>						
-							<strong>网站关键词</strong> （多个关键词请用英文逗号隔开）<br/><label><textarea name="keyword_content"  rows="2"  id="keyword_content" style="width:540px;" ><?php echo($options['keyword_content']); ?></textarea></label>
-							<br/><br/>											
+																	
 							<strong>网站统计代码输入</strong>（添加统计代码，为了不影响主题美观，不被显示是正常现象）<br/>
 							<label><textarea name="footercode"  rows="5"  id="footercode" style="width:540px;"  ><?php echo($options['footercode']); ?></textarea></label>		
 							<br/><br/>											
@@ -248,168 +247,10 @@ $(".toggle").click(function(){$(this).next().slideToggle('slow')});
 										<?php endif ?>
 							</label>
 
-							<br/><label><textarea name="custom_favicon"  rows="1"  id="custom_favicon" style="width:600px;"  ><?php echo($options['custom_favicon']); ?></textarea></label>
-							<br/><br/>   
-							<label><input name="hovertotran" type="checkbox" value="checkbox" <?php if($options['hovertotran']) echo "checked='checked'"; ?> /><strong>开启鼠标悬停时图片变得透明效果</strong>(请不要与下一选项同时开启)</label><br/>													
-							<br/>
-							<label><input name="hovertoclear" type="checkbox" value="checkbox" <?php if($options['hovertoclear']) echo "checked='checked'"; ?> /><strong>开启鼠标悬停时图片变得清晰效果</strong>（请不要与上一选项同时开启）</label><br/>
-							<br/>
-							<label><input name="isie" type="checkbox" value="checkbox" <?php if($options['hovertoclear']) echo "checked='checked'"; ?> /><strong>开启IE升级提示</strong>（推荐Chrome）</label><br/>
-						</div>
+							
 					</fieldset>		
 
-					<fieldset>
-					<legend class="toggle">广告设置</legend>
-						<div class="none">					
-							<label><input name="sidebarad" type="checkbox" value="checkbox" <?php if($options['sidebarad']) echo "checked='checked'"; ?> /><strong>开启边栏广告位(创建广告单元的时候请务必选择158*158大小的)</strong> </label>
-							<br/><br/>（填写广告代码）<br/>
-							<label><textarea name="sidebaradcode"  rows="4"  id="sidebaradcode" style="width:700px;"  ><?php echo($options['sidebaradcode']); ?></textarea></label>
-							<br/>
-						</div>
-					</fieldset>
 					
-					<fieldset>
-					<legend class="toggle">功能设置</legend>
-						<div class="none">																	
-						
-						<label><input name="colorbox" type="checkbox" value="checkbox" <?php if($options['colorbox']) echo "checked='checked'"; ?> /><strong>开启图片点击放大功能</strong></label><br/>													
-						<br/>
-																			
-					
-																			
-						<br/>   
-						<label><input name="glide" type="checkbox" value="checkbox" <?php if($options['glide']) echo "checked='checked'"; ?> /><strong>开启滑动到顶部&底部功能</strong><br/></label>													
-						<br/>
-						
-						<label><input name="thumbnail" type="checkbox" value="checkbox" <?php if($options['thumbnail']) echo "checked='checked'"; ?> /><strong>启用文章缩略图模式</strong></label><br/>		
-						<br/>
-						
-						<label><input name="topnotice" type="checkbox" value="checkbox" <?php if($options['topnotice']) echo "checked='checked'"; ?> /><strong>顶部滚动公告</strong><br/>具体实现方法请参照相关教程</label><br/>		
-						<br/>
-						</div>
-					</fieldset>
-					
-					
-					
-					
-					
-					
-					
-					
-					<fieldset>
-					<legend class="toggle">侧边栏设置</legend>
-						<div class="none">	
-							
-																				
-							
-							<br/>	
-							<label><input name="sidebar_post" type="checkbox" value="checkbox" <?php if($options['sidebar_post']) echo "checked='checked'"; ?> /><strong>开启边栏文章集成小工具</strong></label><br/>													
-							<br/>	
-							
-							<label><input name="sidebar_recentComments" type="checkbox" value="checkbox" <?php if($options['sidebar_recentComments']) echo "checked='checked'"; ?> /><strong>滚动评论</strong></label><br/>													
-							<br/>
-					
-					</fieldset>
-					
-				    <fieldset>
-						<legend class="toggle">模板页面设置</legend>     
-							<div class="none">
-							<br/>
-							
-								<fieldset>
-								<legend class="toggle">关于页面设置</legend>
-									<div class="none">
-										若使用了关于页面模板请在此进行相关设置，提示：关于页面图片的标准尺寸为530 × Y, Y最大值为800<br/><br/>								
-										<strong>标题</strong><br/><label><textarea name="about_title"  rows="1"  id="about_title" style="width:350px;" ><?php echo($options['about_title']); ?></textarea></label><br/><br/>
-										<strong>简介内容</strong><br/><label><textarea name="about_content"  rows="6"  id="about_content" style="width:700px;" ><?php echo($options['about_content']); ?></textarea></label>	<br/><br/>
-										<strong>图片地址</strong><br/><label><textarea name="about_img"  rows="1"  id="about_img" style="width:700px;"><?php echo($options['about_img']); ?></textarea></label>	 																							 
-									</div>
-								</fieldset>
-								
-								<fieldset>
-								<legend class="toggle">微博页面设置</legend>
-									<div class="none">
-										若使用了微博页面模板请在下面输入你获得的代码，提示：宽度请务必选择自适应网页<br/><br/>
-										<label><textarea name="weibocode"  rows="3"  id="weibocode" style="width:700px;" ><?php echo($options['weibocode']); ?></textarea></label>															  
-									</div>
-								</fieldset>
-								
-							</div>
-					</fieldset>
-					
-					
-					
-					<fieldset>
-						<legend class="toggle">SNS网站设置</legend>  						
-							<div class="none">
-							
-								<fieldset>
-																	
-									<label><input name="snsicon" type="checkbox" value="checkbox" <?php if($options['snsicon']) echo "checked='checked'"; ?> /><strong>开启SNS网站显示</strong></label><br/><br/>							
-								</fieldset>
-							
-								<fieldset>
-									<legend class="toggle">SNS详细设置</legend>
-									<div class="none">	
-										<label><input name="rss_select" type="checkbox" value="checkbox" <?php if($options['rss_select']) echo "checked='checked'"; ?> /><strong>开启RSS</strong></label>
-										<br/><br/>		
-										<label><input name="twitter_select" type="checkbox" value="checkbox" <?php if($options['twitter_select']) echo "checked='checked'"; ?> /><strong>开启Twitter图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="twitter_url"  rows="1"  id="twitter_url" style="width:310px;"  ><?php echo($options['twitter_url']); ?></textarea></label>
-										<br/><br/>	 
-										<label><input name="facebook_select" type="checkbox" value="checkbox" <?php if($options['facebook_select']) echo "checked='checked'"; ?> /><strong>开启facebook图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="facebook_url"  rows="1"  id="facebook_url" style="width:310px;"  ><?php echo($options['facebook_url']); ?></textarea></label>                                          								<br/><br/>
-										<label><input name="da_select" type="checkbox" value="checkbox" <?php if($options['da_select']) echo "checked='checked'"; ?> /><strong>开启DeviantART图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="da_url"  rows="1"  id="da_url" style="width:310px;"  ><?php echo($options['da_url']); ?></textarea></label>                                          							
-										<br/><br/>
-										<label><input name="googleplus_select" type="checkbox" value="checkbox" <?php if($options['googleplus_select']) echo "checked='checked'"; ?> /><strong>开启G+图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="googleplus_url"  rows="1"  id="googleplus_url" style="width:310px;"  ><?php echo($options['googleplus_url']); ?></textarea></label>
-										
-										<br/><br/>	 
-										<label><input name="dribbble_select" type="checkbox" value="checkbox" <?php if($options['dribbble_select']) echo "checked='checked'"; ?> /><strong>开启dribbble图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="dribbble_url"  rows="1"  id="dribbble_url" style="width:310px;"  ><?php echo($options['dribbble_url']); ?></textarea></label>                                          							
-										                                          							
-										<br/><br/>
-										<label><input name="flicker_select" type="checkbox" value="checkbox" <?php if($options['flicker_select']) echo "checked='checked'"; ?> /><strong>开启flicker图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="flicker_url"  rows="1"  id="flicker_url" style="width:310px;"  ><?php echo($options['flicker_url']); ?></textarea></label>                                          							
-										<br/><br/>
-										<label><input name="spotify_select" type="checkbox" value="checkbox" <?php if($options['spotify_select']) echo "checked='checked'"; ?> /><strong>开启spotify图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="spotify_url"  rows="1"  id="spotify_url" style="width:310px;"  ><?php echo($options['spotify_url']); ?></textarea></label>                                          							
-										<br/><br/>
-										<label><input name="tumblr_select" type="checkbox" value="checkbox" <?php if($options['tumblr_select']) echo "checked='checked'"; ?> /><strong>开启tumblr图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="tumblr_url"  rows="1"  id="tumblr_url" style="width:310px;"  ><?php echo($options['tumblr_url']); ?></textarea></label>                                          							
-										<br/><br/>
-										<label><input name="weibo_select" type="checkbox" value="checkbox" <?php if($options['weibo_select']) echo "checked='checked'"; ?> /><strong>开启新浪微博图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="weibo_url"  rows="1"  id="weibo_url" style="width:310px;"  ><?php echo($options['weibo_url']); ?></textarea></label>
-										
-										<br/><br/>
-										<label><input name="qqweibo_select" type="checkbox" value="checkbox" <?php if($options['qqweibo_select']) echo "checked='checked'"; ?> /><strong>开启腾讯微博图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="qqweibo_url"  rows="1"  id="qqweibo_url" style="width:310px;"  ><?php echo($options['qqweibo_url']); ?></textarea></label>
-										
-										<br/><br/>
-										<label><input name="kaixin_select" type="checkbox" value="checkbox" <?php if($options['kaixin_select']) echo "checked='checked'"; ?> /><strong>开启开心网图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="kaixin_url"  rows="1"  id="kaixin_url" style="width:310px;"  ><?php echo($options['kaixin_url']); ?></textarea></label>
-										
-										<br/><br/>
-										<label><input name="renren_select" type="checkbox" value="checkbox" <?php if($options['renren_select']) echo "checked='checked'"; ?> /><strong>开启人人网图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="renren_url"  rows="1"  id="renren_url" style="width:310px;"  ><?php echo($options['renren_url']); ?></textarea></label>
-										
-										<br/><br/>
-										<label><input name="douban_select" type="checkbox" value="checkbox" <?php if($options['douban_select']) echo "checked='checked'"; ?> /><strong>开启豆瓣图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="douban_url"  rows="1"  id="douban_url" style="width:310px;"  ><?php echo($options['douban_url']); ?></textarea></label>
-										
-										<br/><br/>
-										<label><input name="sohu_select" type="checkbox" value="checkbox" <?php if($options['sohu_select']) echo "checked='checked'"; ?> /><strong>开启搜狐微博图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="sohu_url"  rows="1"  id="sohu_url" style="width:310px;"  ><?php echo($options['sohu_url']); ?></textarea></label>
-										
-										<br/><br/>
-										<label><input name="wangyi_select" type="checkbox" value="checkbox" <?php if($options['wangyi_select']) echo "checked='checked'"; ?> /><strong>开启网易微博图标</strong>，并填入你的网址（加上http://）</label><br/>
-										<label><textarea name="wangyi_url"  rows="1"  id="wangyi_url" style="width:310px;"  ><?php echo($options['wangyi_url']); ?></textarea></label>
-										
-										<br/><br/>
-									</div>
-								</fieldset>										
-							</div>
-					</fieldset>
 					
 							
 								
